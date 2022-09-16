9c1c0294cccd614753ac11d7129472f0f8a98132
ssusanti8 <susantilalala@gmail.com>
2022-09-17 00:04:59 +0700

    tambah produk

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -10,6 +10,7 @@
 <body style="background: lightgray">
 
 <div class="container mt-5 mb-5">
+<h2> Tambah Isi Produk</h2>
         <div class="row">
             <div class="col-md-12">
                 <div class="card border-0 shadow rounded">
